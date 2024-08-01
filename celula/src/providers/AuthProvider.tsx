2a62e021e0f3442f64d35fc2567153f48d5063c3
Lucas <lgg9083@gmail.com>
2024-08-01 09:32:31 -0300

    feat: adiciona provider de autenticação

---
+++ celula/src/providers/AuthProvider.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect, ReactNode } from "react";
+import AuthContext, { IAuth } from "../context/auth";
+
+const STORAGE_KEY = "token";
+
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [token, setToken] = useState<string | null>(localStorage.getItem(STORAGE_KEY));
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem(STORAGE_KEY);
+    console.log('olha o token', token)
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
+
+  const updateToken = (newToken: string) => {
+    localStorage.setItem(STORAGE_KEY, newToken);
+    setToken(newToken);
+  };
+
+  const logout = () => {
+    localStorage.removeItem(STORAGE_KEY);
+    setToken(null);
+  };
+
+  const contextValue: IAuth = {
+    token,
+    updateToken,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
