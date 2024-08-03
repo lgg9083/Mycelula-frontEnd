4e9c6b3219a8e2f22ced214cbd5d18f0915cfbd8
Lucas <lgg9083@gmail.com>
2024-08-03 19:53:01 -0300

    feat: adiciona campos para cadastrar

--- celula/src/pages/cadastrar/cadastrar.tsx
+++ celula/src/pages/cadastrar/cadastrar.tsx
@@ -1,9 +1,113 @@
-
+import { Button, TextField } from "@mui/material";
+import "./cadastrar.css";
 
 function Cadastrar() {
   return (
-    <div>
-      <p>Hello word</p>
+    <div className=" container">
+      <div className="cadastrar">
+        <form>
+          <div className="listCd">
+            <TextField
+              label="Nome"
+              name="Nome"
+              type="text"
+              variant="outlined"
+              margin="normal"
+              // value={formik.values.senha}
+              // onChange={formik.handleChange}
+              // onBlur={formik.handleBlur}
+              // error={formik.touched.senha && Boolean(formik.errors.senha)}
+              // helperText={formik.touched.senha ? formik.errors.senha || "" : ""}
+            ></TextField>
+            <TextField
+              label="Endereco"
+              name="Endereco"
+              type="text"
+              variant="outlined"
+              margin="normal"
+              // value={formik.values.senha}
+            ></TextField>
+          </div>
+          <div className="listCd">
+            <TextField
+              label="Bairro"
+              name="Bairro"
+              type="Bairro"
+              variant="outlined"
+              margin="normal"
+              // value={formik.values.senha}
+            ></TextField>
+            <TextField
+              label="Data do Batismo"
+              name="Data do Batismo"
+              type="text"
+              variant="outlined"
+              margin="normal"
+              // value={formik.values.senha}
+            ></TextField>
+          </div>
+          <div className="listCd">
+            <TextField
+              label="Data de Nascimento"
+              name="Data de Nascimento"
+              type="text"
+              variant="outlined"
+              margin="normal"
+              // value={formik.values.senha}
+            ></TextField>
+            <TextField
+              label="Telefone"
+              name="Telefone"
+              type="text"
+              variant="outlined"
+              margin="normal"
+              // value={formik.values.senha}
+            ></TextField>
+          </div>
+          <div className="listCd">
+            <TextField
+              label="Cidade"
+              name="Cidade"
+              type="text"
+              variant="outlined"
+              margin="normal"
+              // value={formik.values.senha}
+            ></TextField>
+            <TextField
+              label="Celula"
+              name="Celula"
+              type="text"
+              variant="outlined"
+              margin="normal"
+              // value={formik.values.senha}
+            ></TextField>
+          </div>
+          <div className="listCd">
+            <TextField
+              label="Email"
+              name="Email"
+              type="email"
+              variant="outlined"
+              margin="normal"
+              // value={formik.values.senha}
+            ></TextField>
+            <TextField
+              label="Senha"
+              name="Senha"
+              type="password"
+              variant="outlined"
+              margin="normal"
+              // value={formik.values.senha}
+            ></TextField>
+          </div>
+          <div className="listCd">
+            <Button type="submit" variant="contained">
+              {" "}
+              Cadastrar
+            </Button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
