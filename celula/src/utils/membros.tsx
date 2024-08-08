07a849736d48d576153d4fe180a142dc9ea38f9f
Lucas <lgg9083@gmail.com>
2024-08-08 14:26:33 -0300

    feat : adiciona util com o membro

--- celula/src/utils/membros.tsx
+++ celula/src/utils/membros.tsx
@@ -1,24 +1,31 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { listarMembro } from "../services/routes";
-
+import { Button } from "@mui/material";
+import membroID from "./reuniaoId";
 function Membros() {
   const { data, isError, isPending, isSuccess } = useQuery({
     queryKey: ["membros"],
     queryFn: async () => {
       const response = await listarMembro();
-      console.log(response)
+      console.log(response);
       return response.data;
     },
   });
   return (
     <div className="containerCard">
-      <div>
-       membros
-      </div>
+      {isSuccess &&
+        data.map((membro: any) => (
+          <div key={membro.id} className="card">
+            <p>Nome: {membro.nome}</p>
+            <p>Data de Nascimento: {membro.data_de_nascimento}</p>
+            <p>Endereço: {membro.endereco}</p>
+            <p>Telefone: {membro.telefone}</p>
+            <p>Email: {membro.email}</p>
+          </div>
+        ))}
     </div>
   );
 }
 
-
-export default Membros
+export default Membros;
