75b17ebe98beb1d0d9bfbe64a637cbf4d3236cad
Lucas <lgg9083@gmail.com>
2024-08-11 20:07:53 -0300

    feat: adiciona captura de membros para responsavel pelo quebra gelo  e erros para o input

--- celula/src/pages/reuniao/reuniao.tsx
+++ celula/src/pages/reuniao/reuniao.tsx
@@ -57,7 +57,7 @@ function Reuniao() {
               >
                 <InputLabel>Responsavel Louvor</InputLabel>
                 <Select
-                  name="celula"
+                  name="responsavel_louvor"
                   value={formik.values.responsavel_louvor}
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
@@ -76,12 +76,15 @@ function Reuniao() {
                       ))
                     : null}
                   <MenuItem value="">
-                    <em>Selecione uma membro</em>
+                    <em>Selecione um membro</em>
                   </MenuItem>
                 </Select>
-                {formik.touched.responsavel_louvor && formik.errors.responsavel_louvor && (
-                  <FormHelperText>{formik.errors.responsavel_louvor}</FormHelperText>
-                )}
+                {formik.touched.responsavel_louvor &&
+                  formik.errors.responsavel_louvor && (
+                    <FormHelperText>
+                      {formik.errors.responsavel_louvor}
+                    </FormHelperText>
+                  )}
               </FormControl>
             </div>
             <div className="listCd">
@@ -89,19 +92,24 @@ function Reuniao() {
                 variant="filled"
                 margin="normal"
                 className="custom-textfield"
-                error={formik.touched.responsavel_palavra && Boolean(formik.errors.responsavel_palavra)}
+                error={
+                  formik.touched.responsavel_palavra &&
+                  Boolean(formik.errors.responsavel_palavra)
+                }
                 sx={{ width: "330px" }}
               >
                 <InputLabel>Responsavel Palavra</InputLabel>
                 <Select
                   name="responsavel_palavra"
-                    value={formik.values.responsavel_palavra}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    renderValue={(selected: any): React.ReactNode => {
-                      const selectedItem = data?.find((item: any) => item.id === selected);
-                      return selectedItem ? selectedItem : "Selecione uma Celula";
-                    }}
+                  value={formik.values.responsavel_palavra}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  renderValue={(selected: any): React.ReactNode => {
+                    const selectedItem = data?.find(
+                      (item: any) => item.id === selected
+                    );
+                    return selectedItem ? selectedItem : "Selecione uma Celula";
+                  }}
                 >
                   {data
                     ? data.map((item: any) => (
@@ -114,42 +122,53 @@ function Reuniao() {
                     <em>Selecione um membro</em>
                   </MenuItem>
                 </Select>
-                {formik.touched.responsavel_palavra && formik.errors.responsavel_palavra && (
-                  <FormHelperText>{formik.errors.responsavel_palavra}</FormHelperText>
-                )}
+                {formik.touched.responsavel_palavra &&
+                  formik.errors.responsavel_palavra && (
+                    <FormHelperText>
+                      {formik.errors.responsavel_palavra}
+                    </FormHelperText>
+                  )}
               </FormControl>
               <FormControl
                 variant="filled"
                 margin="normal"
                 className="custom-textfield"
-                // error={formik.touched.celula && Boolean(formik.errors.celula)}
+                error={
+                  formik.touched.responsavel_quebragelo &&
+                  Boolean(formik.errors.responsavel_quebragelo)
+                }
                 sx={{ width: "330px" }}
               >
                 <InputLabel>Responsavel quebra gelo</InputLabel>
                 <Select
-                  name="celula"
-                  //   value={formik.values.celula}
-                  //   onChange={formik.handleChange}
-                  //   onBlur={formik.handleBlur}
-                  //   renderValue={(selected: any): React.ReactNode => {
-                  //     const selectedItem = data?.data?.find((item: any) => item.id === selected);
-                  //     return selectedItem ? selectedItem.nome : "Selecione uma Celula";
-                  //   }}
+                  name="responsavel_quebragelo"
+                  value={formik.values.responsavel_quebragelo}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  renderValue={(selected: any): React.ReactNode => {
+                    const selectedItem = data?.find(
+                      (item: any) => item.id === selected
+                    );
+                    return selectedItem ? selectedItem : "Selecione uma Celula";
+                  }}
                 >
-                  {/* {data
-                    ? data.data.map((item: any) => (
+                  {data
+                    ? data.map((item: any) => (
                         <MenuItem key={item.id} value={item.id}>
                           {item.nome}
                         </MenuItem>
                       ))
                     : null}
                   <MenuItem value="">
-                    <em>Selecione uma Celula</em>
-                  </MenuItem> */}
+                    <em>Selecione um membro</em>
+                  </MenuItem>
                 </Select>
-                {/* {formik.touched.celula && formik.errors.celula && (
-                  <FormHelperText>{formik.errors.celula}</FormHelperText>
-                )} */}
+                {formik.touched.responsavel_quebragelo &&
+                  formik.errors.responsavel_quebragelo && (
+                    <FormHelperText>
+                      {formik.errors.responsavel_quebragelo}
+                    </FormHelperText>
+                  )}
               </FormControl>
             </div>
             <div className="listCd">
@@ -157,22 +176,24 @@ function Reuniao() {
                 variant="filled"
                 margin="normal"
                 className="custom-textfield"
-                // error={formik.touched.celula && Boolean(formik.errors.celula)}
+                error={formik.touched.celula && Boolean(formik.errors.celula)}
                 sx={{ width: "330px" }}
               >
                 <InputLabel>Celula</InputLabel>
                 <Select
                   name="celula"
-                  //   value={formik.values.celula}
-                  //   onChange={formik.handleChange}
-                  //   onBlur={formik.handleBlur}
-                  //   renderValue={(selected: any): React.ReactNode => {
-                  //     const selectedItem = data?.data?.find((item: any) => item.id === selected);
-                  //     return selectedItem ? selectedItem.nome : "Selecione uma Celula";
-                  //   }}
+                  value={formik.values.celula}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  renderValue={(selected: any): React.ReactNode => {
+                    const selectedItem = data?.find(
+                      (item: any) => item.id === selected
+                    );
+                    return selectedItem ? selectedItem : "Selecione uma Celula";
+                  }}
                 >
-                  {/* {data
-                    ? data.data.map((item: any) => (
+                  {data
+                    ? data.map((item: any) => (
                         <MenuItem key={item.id} value={item.id}>
                           {item.nome}
                         </MenuItem>
@@ -180,44 +201,46 @@ function Reuniao() {
                     : null}
                   <MenuItem value="">
                     <em>Selecione uma Celula</em>
-                  </MenuItem> */}
+                  </MenuItem>
                 </Select>
-                {/* {formik.touched.celula && formik.errors.celula && (
+                {formik.touched.celula && formik.errors.celula && (
                   <FormHelperText>{formik.errors.celula}</FormHelperText>
-                )} */}
+                )}
               </FormControl>
               <FormControl
                 variant="filled"
                 margin="normal"
                 className="custom-textfield"
-                // error={formik.touched.celula && Boolean(formik.errors.celula)}
+                error={formik.touched.membros && Boolean(formik.errors.membros)}
                 sx={{ width: "330px" }}
               >
                 <InputLabel> Membros</InputLabel>
                 <Select
-                  name="celula"
-                  //   value={formik.values.celula}
-                  //   onChange={formik.handleChange}
-                  //   onBlur={formik.handleBlur}
-                  //   renderValue={(selected: any): React.ReactNode => {
-                  //     const selectedItem = data?.data?.find((item: any) => item.id === selected);
-                  //     return selectedItem ? selectedItem.nome : "Selecione uma Celula";
-                  //   }}
+                  name="membros"
+                  value={formik.values.membros}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  renderValue={(selected: any): React.ReactNode => {
+                    const selectedItem = data?.find(
+                      (item: any) => item.id === selected
+                    );
+                    return selectedItem ? selectedItem : "Selecione uma Celula";
+                  }}
                 >
-                  {/* {data
-                    ? data.data.map((item: any) => (
+                  {data
+                    ? data.map((item: any) => (
                         <MenuItem key={item.id} value={item.id}>
                           {item.nome}
                         </MenuItem>
                       ))
                     : null}
                   <MenuItem value="">
-                    <em>Selecione uma Celula</em>
-                  </MenuItem> */}
+                    <em>Selecione os participantes</em>
+                  </MenuItem>
                 </Select>
-                {/* {formik.touched.celula && formik.errors.celula && (
-                  <FormHelperText>{formik.errors.celula}</FormHelperText>
-                )} */}
+                {formik.touched.membros && formik.errors.membros && (
+                  <FormHelperText>{formik.errors.membros}</FormHelperText>
+                )}
               </FormControl>
             </div>
           </form>
