c6237d6e7bd0d99db9c7719a4a10a2da4a9192a5
Lucas <lgg9083@gmail.com>
2024-08-12 13:54:06 -0300

    feat: adiciona home ao navbar

--- celula/src/components/NavBar/index.tsx
+++ celula/src/components/NavBar/index.tsx
@@ -1,21 +1,27 @@
 import React from "react";
-import style from './navBar.module.css'
-import logo from '../../images/image.png'
+import style from "./navBar.module.css";
+import logo from "../../images/image.png";
 import { useAuth } from "../../hooks/useAuth";
-function NavBar (){
-    const {logout} = useAuth()
-    return (
-        <nav className={style.navBar}>
-            <div className={style.title}>
-                <img src={logo} alt="logo" />
-                <p>GetCelula</p>
-            </div>
-            <ul>
-             
-                <li onClick={()=> logout()}>Sair</li>
-            </ul>
-        </nav>
-    )
+import { useNavigate } from "react-router-dom";
+function NavBar() {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+  return (
+    <nav className={style.navBar}>
+      <div className={style.title}>
+        <img src={logo} alt="logo" />
+        <p>GetCelula</p>
+      </div>
+      <ul>
+        <li className={style.lisair} onClick={() => navigate("/")}>
+          home
+        </li>
+        <li className={style.lisair} onClick={() => logout()}>
+          Sair
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
