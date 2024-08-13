05069b3c1fc42206182f987eae70b51a6636e340
Lucas <lgg9083@gmail.com>
2024-08-13 13:02:44 -0300

    feat: adiciona formik

--- celula/src/pages/celula/celula.tsx
+++ celula/src/pages/celula/celula.tsx
@@ -8,18 +8,14 @@ import {
   TextField,
 } from "@mui/material";
 import NavBar from "../../components/NavBar";
-import { useFormik } from "formik";
 import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import {
-  buscarCelularId,
-  criarMembro,
-  criarReuniao,
-  IReuniao,
-} from "../../services/routes";
+import { buscarCelularId } from "../../services/routes";
 import { useAuth } from "../../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
+import { useFormik } from "formik";
+import validationSchema from "./validationSchema";
 function Reuniao() {
   const { token, celulaName } = useAuth();
   const { data, isError, isLoading, isSuccess, error } = useQuery({
@@ -30,7 +26,23 @@ function Reuniao() {
       return response;
     },
   });
-
+  const formik = useFormik({
+    initialValues: {
+      nome: "",
+      nome_Lider: "",
+      Bairro: "",
+      Endereco_Da_Celula: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      try {
+        console.log(values);
+        alert("Reuniao cadastrada com sucesso!");
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    },
+  });
   const navigate = useNavigate();
   useEffect(() => {
     if (!token) {
@@ -43,7 +55,7 @@ function Reuniao() {
       <NavBar />
       <div className="container">
         <div className="cadastrarReuniao">
-          <form>
+          <form onSubmit={formik.handleSubmit}>
             <div className="listti">
               <h1>Cadastro de Celula</h1>
               <p>
@@ -61,28 +73,27 @@ function Reuniao() {
                 InputLabelProps={{ shrink: true }}
                 className="custom-textfield"
                 sx={{ width: "330px" }}
-                //   value={formik.values.date || ""}
-                //   onChange={formik.handleChange}
-                //   onBlur={formik.handleBlur}
-                //   error={formik.touched.date && Boolean(formik.errors.date)}
-                //   helperText={formik.touched.date && formik.errors.date}
+                value={formik.values.nome || ""}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.nome && Boolean(formik.errors.nome)}
+                helperText={formik.touched.nome && formik.errors.nome}
               />
               <FormControl
                 variant="filled"
                 margin="normal"
                 className="custom-textfield"
-                //   error={
-                //     formik.touched.responvel_palavra &&
-                //     Boolean(formik.errors.responvel_palavra)
-                //   }
+                error={
+                  formik.touched.nome_Lider && Boolean(formik.errors.nome_Lider)
+                }
                 sx={{ width: "330px" }}
               >
                 <InputLabel>Selecione um membro como Lider</InputLabel>
                 <Select
                   name="nome_Lider"
-                  // value={formik.values.responvel_palavra}
-                  // onChange={formik.handleChange}
-                  // onBlur={formik.handleBlur}
+                  value={formik.values.nome_Lider}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                   renderValue={(selected) => {
                     const selectedMember = data?.Membros.find(
                       (membro: any) => membro.idMembro === selected
@@ -103,13 +114,10 @@ function Reuniao() {
                     <em>Selecione um membro</em>
                   </MenuItem>
                 </Select>
-                {/*   
-                  {formik.touched.responvel_palavra &&
-                    formik.errors.responvel_palavra && (
-                      <FormHelperText>
-                        {formik.errors.responvel_palavra}
-                      </FormHelperText>
-                    )} */}
+
+                {formik.touched.nome_Lider && formik.errors.nome_Lider && (
+                  <FormHelperText>{formik.errors.nome_Lider}</FormHelperText>
+                )}
               </FormControl>
             </div>
             <div className="listCd">
@@ -122,11 +130,11 @@ function Reuniao() {
                 InputLabelProps={{ shrink: true }}
                 className="custom-textfield"
                 sx={{ width: "330px" }}
-                //   value={formik.values.date || ""}
-                //   onChange={formik.handleChange}
-                //   onBlur={formik.handleBlur}
-                //   error={formik.touched.date && Boolean(formik.errors.date)}
-                //   helperText={formik.touched.date && formik.errors.date}
+                value={formik.values.Bairro || ""}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.Bairro && Boolean(formik.errors.Bairro)}
+                helperText={formik.touched.Bairro && formik.errors.Bairro}
               />
               <TextField
                 label="Endereço da celula"
@@ -137,11 +145,17 @@ function Reuniao() {
                 InputLabelProps={{ shrink: true }}
                 className="custom-textfield"
                 sx={{ width: "330px" }}
-                //   value={formik.values.date || ""}
-                //   onChange={formik.handleChange}
-                //   onBlur={formik.handleBlur}
-                //   error={formik.touched.date && Boolean(formik.errors.date)}
-                //   helperText={formik.touched.date && formik.errors.date}
+                value={formik.values.Endereco_Da_Celula || ""}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.Endereco_Da_Celula &&
+                  Boolean(formik.errors.Endereco_Da_Celula)
+                }
+                helperText={
+                  formik.touched.Endereco_Da_Celula &&
+                  formik.errors.Endereco_Da_Celula
+                }
               />
             </div>
 
---
+++ celula/src/pages/celula/validationSchema.tsx
@@ -0,0 +1,13 @@
+import * as yup from "yup";
+
+const validationSchema = yup.object({
+  nome: yup.string().required("O nome é obrigário"),
+  nome_Lider: yup.string().required("O nome do Lider é obrigatório"),
+  Bairro: yup.string().required("O bairro é obrigatório"),
+  Endereco_Da_Celula: yup
+    .string()
+    .required("O endereço da Celula é obrigatório"),
+});
+
+
+export default validationSchema;
