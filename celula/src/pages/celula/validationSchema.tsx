7fe4b42b224cb942ba7038f30de64b33ac5c61c8
Lucas <lgg9083@gmail.com>
2024-08-13 19:57:35 -0300

    fix: muda o nome do endereço no validation

--- celula/src/pages/celula/validationSchema.tsx
+++ celula/src/pages/celula/validationSchema.tsx
@@ -4,7 +4,7 @@ const validationSchema = yup.object({
   nome: yup.string().required("O nome é obrigário"),
   nome_Lider: yup.string().required("O nome do Lider é obrigatório"),
   Bairro: yup.string().required("O bairro é obrigatório"),
-  Endereco_Da_Celula: yup
+  endereco_Da_Celula: yup
     .string()
     .required("O endereço da Celula é obrigatório"),
 });
