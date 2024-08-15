2f76e9667161789a8cae7f37088def2c5919e4a5
Lucas <lgg9083@gmail.com>
2024-08-15 18:44:48 -0300

    feat: adiciona os membros no schema de membros

--- celula/src/pages/celula/validationSchema.tsx
+++ celula/src/pages/celula/validationSchema.tsx
@@ -1,13 +1,14 @@
 import * as yup from "yup";
+import Membros from "../../utils/membros";
 
 const validationSchema = yup.object({
   nome: yup.string().required("O nome é obrigário"),
   nome_Lider: yup.string().required("O nome do Lider é obrigatório"),
   Bairro: yup.string().required("O bairro é obrigatório"),
-  endereco_Da_Celula: yup
+  Endereco_Da_Celula: yup
     .string()
     .required("O endereço da Celula é obrigatório"),
+  Membros: yup.array(),
 });
-
 
 export default validationSchema;
